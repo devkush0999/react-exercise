6858eec5ab9833bc3c1756d2a08868bfb72bee02
devesh0999 <devesh2cse@gmail.com>
2024-09-25 23:36:53 +0530

    addd

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,13 @@
 // Create a form that takes user input and displays it in real-time
 
+import './../main.css'
 import React, { useState } from 'react'
 
 function Form() {
     const  [input, setInput] = React.useState('');
   return (
-    <div>
-        <input type="text" value={input} onChange={(e)=>setInput(e.target.value)}/>
+    <div className='bg-yellow-300'>
+        <input type="text" className='border border-green-400 bg-yellow-300' value={input} onChange={(e)=>setInput(e.target.value)}/>
         <p>User Input : {input}</p>
         
     </div>
